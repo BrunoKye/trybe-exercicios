7cefb42cb8d08b9b49254b587d6a04b3d24a214a
Bruno Ferreira <contatobrunokye@gmail.com>
2022-08-18 19:08:37 -0300

    Adiciona programa que retorna maior numero

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/2-maiorNumero.js
@@ -0,0 +1,13 @@
+let a = 7212;
+let b = 323;
+
+if(a > b){
+    console.log('O número', a, 'é maior.');
+}
+else if(b > a){
+    console.log('O número', b, 'é maior.');
+}
+else {
+    console.log('Os números são iguais.');
+}
+
