bc98ede4634d99dedc77902623573abc9a0c43a3
Bruno Ferreira <contatobrunokye@gmail.com>
2022-08-19 13:57:28 -0300

    Adiciona programa que identifica numero par

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/8-identificaPar.js
@@ -0,0 +1,12 @@
+const a = 1;
+const b = 1232;
+const c = 31;
+
+if(a % 2 === 0 || b % 2 === 0 || c % 2 === 0){
+    console.log('true');
+}
+else{
+    console.log('false');
+}
+
+/* corrigir para retornar Boolean */
