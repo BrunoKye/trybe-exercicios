b8a5c1df757a225b8bdc469419a79deffad0f15e
Bruno Ferreira <contatobrunokye@gmail.com>
2022-08-19 14:31:55 -0300

    Corrige variaveis do programa que calcula lucro

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/10-calculaLucro.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/10-calculaLucro.js
@@ -1,14 +1,14 @@
-const custo = 100;
-const venda = 350;
-let custoTotal = custo + (custo * 0.20);
-let lucro = (venda - custoTotal);
+const valorCusto = 100;
+const valorVenda = 350;
+let custoTotal = valorCusto + (valorCusto * 0.20);
+let valorLucro = (valorVenda - custoTotal);
 
-if (custo < 0 || venda < 0){
+if (valorCusto < 0 || valorVenda < 0){
     console.log('ERRO: Valor negativo.');
 }
-else if(custoTotal > venda){
+else if(custoTotal > valorVenda){
     console.log('ERRO: Custo total maior do que o valor de venda.');
 }
 else {
-    console.log('A venda de mil unidades do produto resultou em um lucro de: R$', lucro * 1000 ,'reais');
+    console.log('A venda de mil unidades do produto resultou em um lucro de: R$', valorLucro * 1000 ,'reais');
 }
