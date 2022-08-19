4d7ff8f99284a6802fa848453b4bcf06cc7a30b3
Bruno Ferreira <contatobrunokye@gmail.com>
2022-08-19 13:59:34 -0300

    Adiciona programa que identifica numero impar

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/9-identificaImpar.js
@@ -0,0 +1,12 @@
+const a = 2;
+const b = 1231;
+const c = 32;
+
+if(a % 2 === 1 || b % 2 === 1 || c % 2 === 1){
+    console.log('true');
+}
+else{
+    console.log('false');
+}
+
+/* corrigir para retornar Boolean */
