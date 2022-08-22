dc061965dab1015cdf16143872b479cf624b3778
Bruno Ferreira <contatobrunokye@gmail.com>
2022-08-22 13:20:31 -0300

    Ordena o array de forma decrescente

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios/bonus.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios/bonus.js
@@ -1,4 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let crescent = numbers.sort((a, b) => a - b);
+// let crescent = numbers.sort((a, b) => a - b);
+let decrescent = numbers.sort((a, b) => b - a);
 
-console.log(crescent);
+// console.log(crescent);
+console.log(decrescent);
