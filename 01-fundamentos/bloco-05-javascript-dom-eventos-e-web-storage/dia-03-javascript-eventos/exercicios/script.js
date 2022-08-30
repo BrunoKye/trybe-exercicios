1acc86a43793ed852e7f150f7bba48713d1811c0
Bruno Ferreira <contatobrunokye@gmail.com>
2022-08-30 11:44:36 -0300

    Adiciona dias do calendario

--- 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicios/script.js
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicios/script.js
@@ -12,24 +12,11 @@ function createDaysOfTheWeek() {
 };
   
 createDaysOfTheWeek();
-  
+
+// :rocket:
+
+// Exercício 1: O array decemberDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Sua função deve criar dinamicamente cada dia do calendário e os adicionar dentro da tag <ul>.
 let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-
-function dayHoli() {
-    for (let i = 0; i < decemberDaysList.length; i += 1) {
-        if (decemberDaysList[i] === (24 || 25 || 31)) {
-            return 'holiday';
-        }
-    }
-}
-
-function daySun() {
-  for (let i = 0; i < decemberDaysList.length; i += 1) {
-    if (decemberDaysList[i] === (4 || 11 || 18 || 25)) {
-      return 'sunday';
-    }
-  }
-}
 
 function calendarDays() {
   for (let i = 0; i < decemberDaysList.length; i += 1) {
@@ -39,10 +26,10 @@ function calendarDays() {
     ulDays.appendChild(liDays);
     liDays.classList = "days";
 
-    if (dayHoli === "holiday") {
+    if (decemberDaysList[i] === 24 || decemberDaysList[i] === 25 || decemberDaysList[i] === 31){
         liDays.classList.add("holiday");
     }
-    if (daySun === "sunday") {
+    if (decemberDaysList[i] === 4 || decemberDaysList[i] === 11 || decemberDaysList[i] === 18 || decemberDaysList[i] === 25) {
         liDays.classList.add("friday");
     }
 
