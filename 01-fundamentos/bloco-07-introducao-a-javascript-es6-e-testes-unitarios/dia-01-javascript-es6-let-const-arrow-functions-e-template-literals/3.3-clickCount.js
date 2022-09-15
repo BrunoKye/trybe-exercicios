da679ea8e118d8be01813ccc3c1e9f52647391d2
Bruno Ferreira <contatobrunokye@gmail.com>
2022-09-15 17:52:56 -0300

    Adiciona exercicio 3 pt.2

---
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/3.3-clickCount.js
@@ -0,0 +1,10 @@
+const btnClick = document.getElementById('btn-click');
+const counter = document.getElementById('counter');
+let clicks = 0;
+
+const clickCount = () => {
+    clicks += 1;
+    counter.innerHTML = clicks;
+}
+
+btnClick.addEventListener('click', clickCount)
