41a2f19e51a2e3369d34960942a33c7e4e3495e6
Bruno Ferreira <contatobrunokye@gmail.com>
2022-09-16 20:09:05 -0300

    Corrige commits

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/2.1-templateLiterals.js
+++
@@ -1,4 +0,0 @@
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
-const sortOddsAndEvens = () => oddsAndEvens.sort((a, b) => a - b);
-
-console.log(`Os números ${sortOddsAndEvens()} se encontram ordenados de forma crescente.`);
--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/2.2-longestWord.js
+++
@@ -1,7 +0,0 @@
-const longestWord = phrase => {
-    let findWord = phrase.split(' ').sort((a, b) => b.length - a.length);
-    
-    return findWord[0];
-}
-
-console.log(longestWord('Antônio foi ao banheiro e não sabemos o que aconteceu.'));
