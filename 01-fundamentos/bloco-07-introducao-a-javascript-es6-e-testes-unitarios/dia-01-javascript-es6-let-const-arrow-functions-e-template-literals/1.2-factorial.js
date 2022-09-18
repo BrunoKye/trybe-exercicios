f354a3b104b6c9ef0c9c9c538723de1e413712d1
Bruno Ferreira <contatobrunokye@gmail.com>
2022-09-18 19:07:08 -0300

    Revert "Exercícios 7.2 - JavaScript ES6: let, const, arrow functions e template literals"

---
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/1.2-factorial.js
@@ -0,0 +1,14 @@
+const calculateFactorial = n => {
+    let result = 1;
+
+    if (n > 1) {
+        for (let i = 2; i <= n; i += 1) {
+            result *= i;
+        }
+    } else {
+        result = 'o número é inválido.';
+    }
+    return result;
+}
+
+console.log(`Esse é o fatorial: ${calculateFactorial(5)}`);
