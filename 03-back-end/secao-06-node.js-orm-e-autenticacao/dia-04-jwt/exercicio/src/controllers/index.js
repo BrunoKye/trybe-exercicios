110eb547692eea280602416bb566dc4e01ade965
Bruno Ferreira <contatobrunokye@gmail.com>
2023-04-12 17:53:41 -0300

    feat: back-end 6.4

---
+++ 03-back-end/secao-06-node.js-orm-e-autenticacao/dia-04-jwt/exercicio/src/controllers/index.js
@@ -0,0 +1,13 @@
+const login = require('./login');
+const ping = require('./ping');
+const me = require('./me');
+const topSecret = require('./topSecret');
+const signup = require('./signup');
+
+module.exports = {
+  login,
+  ping,
+  me,
+  topSecret,
+  signup,
+};
