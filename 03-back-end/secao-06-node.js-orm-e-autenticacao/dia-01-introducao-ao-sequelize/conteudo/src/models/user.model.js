7756970970bd1d976ab04193dd91156dd37778e3
Bruno Ferreira <contatobrunokye@gmail.com>
2023-04-12 17:48:11 -0300

    feat: back-end 6.1

---
+++ 03-back-end/secao-06-node.js-orm-e-autenticacao/dia-01-introducao-ao-sequelize/conteudo/src/models/user.model.js
@@ -0,0 +1,9 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    fullName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phoneNum: DataTypes.STRING,
+  });
+
+  return User;
+};
---
+++ 03-back-end/secao-06-node.js-orm-e-autenticacao/dia-01-introducao-ao-sequelize/exercicio/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const Book = require('./controllers/book.controller');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/books', Book.getAll);
+app.get("/books/:id", Book.getById);
+
+app.post('/books', Book.create);
+app.put('/books/:id', Book.update);
+app.delete('/books/:id', Book.remove);
+
+module.exports = app;
