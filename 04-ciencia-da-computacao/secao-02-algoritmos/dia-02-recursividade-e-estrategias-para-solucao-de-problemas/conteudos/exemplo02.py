106f8f5a7f28a53e8816d2fe18a55fd06b7c1e71
Bruno Ferreira <contatobrunokye@gmail.com>
2023-06-20 01:04:20 -0300

    feat: cs 2.2

---
+++ 04-ciencia-da-computacao/secao-02-algoritmos/dia-02-recursividade-e-estrategias-para-solucao-de-problemas/conteudos/exemplo02.py
@@ -0,0 +1,18 @@
+# Pilha de Camadas
+
+def saudacao():
+    print("Oi")
+
+
+def despedida():
+    print("Tchau")
+
+
+def init():
+    print("Inicio")
+    saudacao()
+    print("Fim")
+    despedida()
+
+
+init()
---
+++ 04-ciencia-da-computacao/secao-02-algoritmos/dia-02-recursividade-e-estrategias-para-solucao-de-problemas/exercicios/exercicio00.py
@@ -0,0 +1,23 @@
+# 01 - Iterativo
+
+def conta_pares(n):
+    numero_de_pares = 0
+    for num in range(n+1):
+        if num % 2 == 0 and num != 0:
+            numero_de_pares += 1
+    return numero_de_pares
+
+
+# 02 - Recursivo
+
+# def conta_pares(n):
+#     if n == 1:
+#         return 0
+#     elif n % 2 == 0:
+#         return 1 + conta_pares(n-1)
+#     else:
+#         return conta_pares(n-1)
+
+
+result = conta_pares(10)
+print(result)
